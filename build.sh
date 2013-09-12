42970754306194d0f42d1c0781b63bca67ff9e17
Neal Todd <neal.todd@torchbox.com>
2013-09-12 11:01:00 +0100

    Updated test url to retrieve raw test image

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ function msg {
     echo "Thumbor box created."
 
     echo "Test with vagrant up then:"
-    echo "http://localhost:8888/unsafe/300x/https://github.com/globocom/thumbor/blob/master/tests/visual_test/flower.jpg"
+    echo "http://localhost:8888/unsafe/300x/https://raw.github.com/globocom/thumbor/blob/master/tests/visual_test/flower.jpg"
     echo "Install box locally with:"
     echo "vagrant box add $BOX_NAME $BOX" 
 }
